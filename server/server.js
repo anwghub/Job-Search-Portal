23c7244f5343e7a7ab93d1076102cf2345a37ddf
Anwesa Ghosh <anwesa2021@gmail.com>
2025-06-18 01:38:41 +0530

    server added

--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connect from './db/connect.js'; 
 import fs from 'fs';
+import User from './models/UserModel.js';
+import asyncHandler from 'express-async-handler';
 
 dotenv.config();
 
 const app = express();
-
-const port = 8000;
 
 
 const config = {
@@ -29,16 +29,49 @@ app.use(
     })
 );
 
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(auth(config));
 
-app.get('/random', (req, res) => {
-    res.json({random : Math.random()});
+//function to check if user is existed in the db
+const enusureUserInDB = asyncHandler(async (user) => {
+  try {
+    const existingUser = await User.findOne({ auth0Id: user.sub });
+
+    if (!existingUser) {
+      // create a new user document
+      const newUser = new User({
+        auth0Id: user.sub,
+        email: user.email,
+        name: user.name,
+        role: "jobseeker",
+        profilePicture: user.picture,
+      });
+
+      await newUser.save();
+
+      console.log("User added to db", user);
+    } else {
+      console.log("User already exists in db", existingUser);
+    }
+  } catch (error) {
+    console.log("Error checking or adding user to db", error.message);
+  }
 });
+
+app.get("/", async (req, res) => {
+    if (req.oidc.isAuthenticated()) {
+        // If user is authenticated, ensure they are in the database
+        await enusureUserInDB(req.oidc.user);
+        return res.redirect(process.env.CLIENT_URL);
+    } else {
+        // If not authenticated, return false
+        return res.send("Logged out");
+    }
+});
+
 
 //routes
 const routeFiles = fs.readdirSync("./routes");
@@ -46,7 +79,7 @@ const routeFiles = fs.readdirSync("./routes");
 routeFiles.forEach((file) => {
     //import dynamic routes
     import(`./routes/${file}`).then((route) => {
-        app.use(route.default);
+        app.use("/api/v1/",route.default);
     }).catch((err) => {
         console.error(`Error loading route ${file}:`, err);
     });
@@ -56,7 +89,7 @@ const server = async () => {
     try {
         await connect();
         app.listen(process.env.PORT, () => {
-            console.log(`Server is listening to the ${port}`);
+            console.log(`Server is listening to the port: ${process.env.PORT}`);
         })
     } catch (error) {
         console.log("Server error",error.message);
